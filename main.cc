8eb50757a51a44daf310fce03037e234dc1e2ceb
Eric Huang <eric.haoran.huang@gmail.com>
2022-06-19 14:49:31 -0400

    Starting to add changes to parser

--- main.cc
+++ main.cc
@@ -1,12 +1,20 @@
 #include <iostream>
 #include <string>
+#include "parser/parser.h"
+
+#define SIZE 2000000
 
 using namespace std;
 
-int main() {
+int main(int argc, char *argv[]) {
 
+	// this represents our memory:
+	uint32_t memory [SIZE];
 
-
-
+	// Step 1: Transform .pun to binary, and load the binary into memory
+	// Step 2: Have a program counter to point to instruction we take out
+		// Then, we decode it (into a specific instruction)
+		// Then we execute that instruction
+	Parser::writePUNInstructionsToMemory(, memory);
 	return 0;
 }
---
+++ parser/parser.h
@@ -0,0 +1,11 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include <string>
+using namespace std;
+
+namespace Parser {
+void writePUNInstructionsToMemory(string filename, int *memory);
+};
+
+#endif
