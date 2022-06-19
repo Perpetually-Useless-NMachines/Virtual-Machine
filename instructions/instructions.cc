cd957ee1156e3fa4d9e04afaf87ab6f287a60c6f
j2447zha <j2447zha@uwaterloo.ca>
2022-06-19 14:50:54 -0400

    constructor done youpee

--- instructions/instructions.cc
+++ instructions/instructions.cc
@@ -1,7 +1,11 @@
 #include "instructions.h"
 
 // TODO: Jenny pls implement this constructor:
-Instructions::Instructions(int reg1, int reg2, int reg3) {}
+Instructions::Instructions(int reg1, int reg2, int reg3) {
+    this.reg1 = reg1;
+    this.reg2 = reg2;
+    this.reg3 = reg3;
+}
 
 // TODO: Let's parse this instruction together:
 Instructions parseInstruction(string s) {}
