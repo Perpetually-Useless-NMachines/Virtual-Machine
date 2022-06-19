ae8d98c2ded291c92e4e0b852485bf8aabd37aad
Eric Huang <eric.haoran.huang@gmail.com>
2022-06-19 15:16:33 -0400

    Add in file readability checks

--- parser/parser.cc
+++ parser/parser.cc
@@ -1,7 +1,31 @@
 #include "parser.h"
 #include <iostream>
+#include <cstdlib>
+#include <string>
+#include <sys/wait.h>
+#include <fstream>
+#include <sstream>
 using namespace std;
 
+// checks if a file exists and returns exit status, 0 means it does, 1 means it doesn't
+bool doesFileExist(const string &filename) {
+  return WEXITSTATUS(system(("test -f " + filename).c_str())) == 0;
+}
+
+bool isOutputFileReadable(const string &outputFileName) {
+  if (!doesFileExist(outputFileName)) {
+    cerr << "Output File: " << outputFileName << " does not exist!" << endl;
+    throw "file doesn't exist";
+  }
+  return true;
+}
+
 void Parser::writePUNInstructionsToMemory(string filename, uint32_t *memory) {
-    cout << filename << endl; 
+  cout << filename << endl;
+  if (isOutputFileReadable(filename)) {
+    cout << "readable" << endl;
+  }
+
+  
+
 }
