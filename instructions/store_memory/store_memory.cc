9ecb5c49bf4507a8cda054815406edd3793aeb01
Eric Huang <eric.haoran.huang@gmail.com>
2022-07-31 09:51:18 -0400

    Start of parsing the rest of the registers

--- instructions/store_memory/store_memory.cc
+++ instructions/store_memory/store_memory.cc
@@ -1,4 +1,6 @@
 #include "store_memory.h"
+#include <iostream>
+using namespace std;
 
 StoreMemory::StoreMemory(int reg1, int reg2, int reg3) {
   this->reg1 = reg1;
@@ -9,7 +11,8 @@ StoreMemory::StoreMemory(int reg1, int reg2, int reg3) {
 // given a string rest (so in this case it'll be like 10, 20, 30), either throw an error for a mismatched string
 // or properly construct StoreMemory
 StoreMemory::StoreMemory(const string &s) {
-
+  cout << "Rest of the string: " << s << endl;
+  // "0, 0, 100" -- parse it so that reg1 = 0, reg2 = 0, reg3 = 100
 }
 
 void StoreMemory::performInstruction() {
